1d2877ceca235dfa7541689c561addc321f2e708
Skylar Douglas Hoffert <skylarh@vt.edu>
2017-06-08 17:36:44 -0400

    Glider and Container distinction in rx

--- groundstation/GUI/ReceiveData.m
+++ groundstation/GUI/ReceiveData.m
@@ -5,8 +5,9 @@
 % Modified By:  Sky Hoffert
 % LastModified: 6/8/2017
 
-% filename
-filename = 'log.csv'
+% filenames
+filename_glider = 'log_glider.csv';
+filename_container = 'log_container.csv';
 
 % close the old serial connection if it is open already
 if exist('serialCon', 'var')
@@ -36,23 +37,48 @@ isRunning = true;
 while isRunning
     % get the data from the UART connection
     data = fscanf(serialCon)
+    
+    % check the data
     if ~isempty(data)
-        data_matrix(1) = str2num(data(1:4));
-        data_matrix(2) = str2num(data(6:9));
-        data_matrix(3) = str2num(data(11:14));
-        data_matrix(4) = str2num(data(16:19));
-        data_matrix(5) = str2num(data(21:24));
-        data_matrix(6) = str2num(data(26:29));
-        data_matrix(7) = str2num(data(31:34));
-        data_matrix(8) = str2num(data(36:39));
-        data_matrix(9) = str2num(data(41:44));
-        data_matrix(10) = str2num(data(46:49));
+        % decide if glider or container data
+        if (strcmp(data(6:11), 'GLIDER'))
+            % glider collection
+            data_matrix(1) = str2num(data(1:4)); % teamid
+            data_matrix(2) = data(6:11); % origin
+            data_matrix(3) = str2num(data(13:16)); % mission time
+            data_matrix(4) = str2num(data(18:21)); % packet count
+            data_matrix(5) = str2num(data(23:26)); % altitude
+            data_matrix(6) = str2num(data(28:31)); % pressure
+            data_matrix(7) = str2num(data(33:36)); % speed
+            data_matrix(8) = str2num(data(38:41)); % temp
+            data_matrix(9) = str2num(data(43:46)); % voltage
+            data_matrix(10) = str2num(data(48:51)); % heading
+            data_matrix(11) = str2num(data(48:51)); % sw state
+            
+            % append data to the save file
+            dlmwrite(filename_glider, data_matrix, 'delimiter', ',', '-append');
+        elseif (strcmp(data(6:14), 'CONTAINER'))
+            % container collection
+            data_matrix(1) = str2num(data(1:4)); % teamid
+            data_matrix(2) = data(6:14); % origin
+            data_matrix(3) = str2num(data(15:18)); % mission time
+            data_matrix(4) = str2num(data(20:23)); % packet count
+            data_matrix(5) = str2num(data(25:28)); % altitude
+            data_matrix(6) = str2num(data(30:33)); % temperature
+            data_matrix(7) = str2num(data(35:38)); % voltage
+            data_matrix(8) = str2num(data(40:43)); % sw state
+            
+            % append data to the save file
+            dlmwrite(filename_container, data_matrix, 'delimiter', ',', '-append');
+        end
     else
-        data_matrix = [0 0 0 0 0 0 0 0 0 0];
+        % empty data
+        data_matrix = {0 0 0 0 0 0 0 0 0 0 0};
     end
     
-    % append data to the save file
-    dlmwrite(filename, data_matrix, 'delimiter', ',', '-append');
+    % allow a transmission to be complete
+    % this may need to be removed
+    pause(.333);
 end
 
 % close the connection for future use
